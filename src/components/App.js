f21bb4000bd086e2019ff7ec9925135eebc860e1
MiriamPaternain <miriampaternain@gmail.com>
2023-09-07 10:53:15 +0200

    add Preview component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import '../styles/App.scss';
 import cover from '../images/cover.jpeg';
 import user from '../images/user.jpeg';
 import Header from './Header';
+import Preview from './Preview';
+
+
 function App() {
   //Variables de estado
 
@@ -44,40 +47,8 @@ function App() {
     <div>
       <div className='main'>
         <Header />
+        <Preview />
         
-        <section className='preview'>
-          <img className='preview_coverImage' src={cover} alt='' />
-
-          <section className='preview_author'>
-            <section className='preview_author--personalInfo'>
-              <img className='preview_author--image' src={user} alt='' />
-              <p className='preview_author--job'>
-                {data.job || 'Puesto de trabajo'}
-              </p>
-              <p className='preview_author--name'>{data.author || 'Nombre'}</p>
-            </section>
-            <section className='preview_project'>
-              <p className='preview_project--subtitle'>Personal Project Card</p>
-              <hr className='preview_project--line' />
-
-              <h2 className='preview_project--title'>
-                {data.projectName || 'Nombre del proyecto'}
-              </h2>
-              <p className='preview_project--slogan'>
-                {data.slogan || 'Slogan'}
-              </p>
-              <p className='preview_project--description'>
-                {data.desc ||
-                  'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Libero, delectus? Voluptates at hic aliquam porro ad suscipitharum laboriosam saepe earum doloribus aperiam, ullam culpa accusantium placeat odit corrupti ipsum!'}
-              </p>
-              <section className='preview_technologies'>
-                <p className='preview_project--technologiesText'>
-                  {data.technologies || 'Tecnologías'}
-                </p>
-              </section>
-            </section>
-          </section>
-        </section>
 
         <section className='form'>
           <h2 className='form_title'>Información</h2>
