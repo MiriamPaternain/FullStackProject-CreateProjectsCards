1c9033797d0f9ee444f031dfc3ce719ae2f6c8aa
MiriamPaternain <miriampaternain@gmail.com>
2023-09-11 11:10:22 +0200

    create data base connection

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ const server = express();
 
 server.use(cors());
 server.use(express.json({limit:"100mb"}));
+dotenv.config();
 
-const PORT = 4000;
+mysql.createConnection({
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    password: process.env.PASS,
+});
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, ()=>{
     console.log("Se ha conectado al puerto" + PORT);
 });
